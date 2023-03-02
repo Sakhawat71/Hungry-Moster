11e842dc59dbff819cf795d9750415fd4d8a1706
sakhawat71 <sakhawat.showrav@gmail.com>
2023-03-02 17:23:36 +0600

    25%

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("start work"); 
+// const getMeleInfo = mealID =>{
+
+//     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`
+//     fetch(url)
+//     .then(res => res.json())
+//     .then(data => console.log(data));
+// }
+
+
+// fetch('www.themealdb.com/api/json/v1/1/random.php')
+// .then(res => res.json())
+// .then(data => console.log(data));
+
