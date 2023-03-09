53d6d597f1584387d9cba3dcf80871faa0caed32
sakhawat71 <sakhawat.showrav@gmail.com>
2023-03-09 12:13:08 +0600

    work on search part

--- app.js
+++ app.js
@@ -1,13 +1,48 @@
-// const getMeleInfo = mealID =>{
 
-//     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`
-//     fetch(url)
-//     .then(res => res.json())
-//     .then(data => console.log(data));
-// }
+// function test(){
+//     const click = document.getElementById("meal-submit");
+//     const value = document.getElementById("meal-input").value;
+//     click.addEventListener("click", function(){
+//         console.log(value);
+//         console.log("clicked");
+//     })
+// };
+
+// test();
+
+// const url = `https://www.themealdb.com/api/json/v1/1/search.php?s= ${mealInput}`
+//         fetch(url)
+//         .then(res => res.json())
+//         .then(data => {
+//             console.log(data);
+//         })
+const showMatchingMealInfo =(data,mealInput) =>{
+
+}
 
 
-// fetch('www.themealdb.com/api/json/v1/1/random.php')
-// .then(res => res.json())
-// .then(data => console.log(data));
+const searchMeal = () => {
+    const mealInput = document.getElementById("meal-input").value;
+    
+    if(mealInput){
+        const noMealFound = document.getElementById('no-meal-found');
+        noMealFound.innerText = ``;
+        const mealInfoSection = document.getElementById('meal-info-section');
+        mealInfoSection.innerHTML = ``;
+        const mealDetailsSection = document.getElementById('meal-details-section');
+        mealDetailsSection.innerHTML = ``;
 
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s= ${mealInput}`
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            showMatchingMealInfo(data,mealInput);
+        })
+    }
+    else{
+        const noMealFound = document.getElementById("no-meal-found");
+        noMealFound.innerText = `Type something inside the search bar`;
+    }
+}
+
+document.getElementById("meal-submit").addEventListener("click",searchMeal);
