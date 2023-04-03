dd28954da8c848468eed4d3dd6cb76a15867c91d
sakhawat71 <sakhawat.showrav@gmail.com>
2023-04-04 02:40:15 +0600

    almost done

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
 
-// const searchItem = async() => {
-//     const searchInput = document.getElementById("search-input").value;
-//     try{
-//         const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealID}`;
-//         const res = await fetch(url);
-//         const allData =await res.json();
-//         displayItem(allData.data);
-//     }
-//     catch(error){
-//         errorShow("sorry something is worong !");
-//     }
-
-// }
 
 const searchItem = () => {
-    const mealID = document.getElementById("search-input").value;
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealID}`;
+    const mealName = document.getElementById("search-input").value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`;
     fetch(url)
     .then(res => res.json())
     .then(meals => displayMealItem(meals.meals));
@@ -25,14 +12,16 @@ const displayMealItem = meals =>{
 
     const showSection = document.getElementById("show-search-item");
     showSection.innerText = ``;
+    const MealDetails = document.getElementById("Meal-details-section");
+    MealDetails.innerText = ``;
 
     meals.forEach(meals => {
         const mealName = meals.strMeal;
         const mealPic =  meals.strMealThumb;
-
+        const mealID = meals.idMeal;
         const mealInfo = `
-        <a href="">
-        <div class="card cursor costom-card shadow">
+        <a href="#Meal-details-section">
+        <div onclick="showItemDetails('${mealID}')" class="card cursor costom-card shadow">
           <img class="card-pic" style="width: 260px; height: 200px; border-radius: 10px 10px 0 0" src="${mealPic}">
           <div class="card-body">
             <h5 class="card-title text-center">${mealName}</h5>
@@ -45,6 +34,54 @@ const displayMealItem = meals =>{
         searchResultDiv.className = 'search-div justify-content-center mb-3 p-0 d-flex col-xm-1 col-sm-1 col-md-3';
         searchResultDiv.innerHTML = mealInfo;
         showSection.appendChild(searchResultDiv);
-
     });
 }
+
+
+
+
+
+// ---------------------- Show meal details --------------------
+
+
+
+
+
+const showItemDetails = mealID => {
+
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(mealArray => showMealDetails(mealArray.meals));
+
+}
+
+const showMealDetails = mealsingel =>{
+
+    const meal = mealsingel[0];
+    const mealPic = meal.strMealThumb;
+    const mealName = meal.strMeal;
+    
+    const mealDetails = document.getElementById("Meal-details-section");
+    mealDetails.innerHTML = `
+    <div id="meal-info" class="card px-0 pb-1 border-0 shadow col-xm-12 col-sm-12 col-md-6">
+            
+    <img id="meal-img" src="${mealPic}" class="card-img-top" style="border-radius: 10px 10px 0 0;">
+    
+    <div class="card-body">
+        <h2 class="card-title text-center my-3">${mealName}</h2>
+        <hr>
+        <h4 class="card-title mt-4">Meal Ingredients</h4>
+        <div id="meal-ingredients"></div>
+    </div>
+  </div>
+    `;
+
+    const Ingredients = document.getElementById("meal-ingredients");
+    // meal.forEach(element => {
+    //     console.log(element);
+    // });
+    const x = meal.strIngredient1;
+    const y = meal.strMeasure1;
+    console.log(y,x);
+};
